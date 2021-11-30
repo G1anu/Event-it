fd744e974062caa5d55a03108004a86c59ab26ca
bautistamasssanti <bautista.massanti@ing.austral.edu.ar>
2021-11-30 11:03:58 -0300

    agregue el link del informe

--- Testing/Testeo_Manual.py
+++ Testing/Testeo_Manual.py
@@ -5,7 +5,8 @@ from Anses.Anses import leerCuilsDelAnses
 from Anses.Anses import leerDataEspecificaDeCiudadano
 
 
-#esto lo usamos para testear por el camino que los metodos funcionaran
+#esto lo usamos para testear por el camino que los metodos funcionaran,no tiene mayor importancia
+
 #ciudadano1 = Ciudadano(23440529259,1154885993)
 #ciudadano2 = Ciudadano(13903989504,1188549359)
 #escribirCiudadano(ciudadano1)
@@ -21,22 +22,21 @@ from Anses.Anses import leerDataEspecificaDeCiudadano
 #listaCUILMacumbero = []
 #leerDataEspecificaDeCiudadano(listaCUILMacumbero,0)
 #print(listaCUILMacumbero)
-from Mapa.Mapa import mapa
-
-try:
-    x= int(input("ingrese el valor: "))
-    if x == 1:
-        print("membrillo1")
-    elif x == 2:
-        print("membrillo2")
-    elif x == 3:
-        print("membrillo3")
-    elif x == 4:
-        mapa()
-    elif x == 5:
-        print("membrillo5")
-    else:
-        print("dulce de batata")
-except ValueError:
-    print("salamin con queso")
+#from Mapa.Mapa import mapa
+#try:
+#    x= int(input("ingrese el valor: "))
+#    if x == 1:
+#        print("membrillo1")
+#    elif x == 2:
+#        print("membrillo2")
+#    elif x == 3:
+#        print("membrillo3")
+#    elif x == 4:
+#        mapa()
+#    elif x == 5:
+#        print("membrillo5")
+#    else:
+#        print("dulce de batata")
+#except ValueError:
+#    print("salamin con queso")
 
